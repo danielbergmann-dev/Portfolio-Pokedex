8db58fcb6544d5ed23c5d19d9aac3251bbeaf6b9
Daniel Bergmann <105534792+danielbergmann-dev@users.noreply.github.com>
2023-06-15 14:40:46 +0200

    t

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const getPokemon = async id => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const pokemon = await res.json();
-  console.log(pokemon);
+/*   console.log(pokemon); */
   createPokemonCard(pokemon);
 };
 
